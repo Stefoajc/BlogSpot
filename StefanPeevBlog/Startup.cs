5ae93705ee4e364d5072a12ce56fd1cd7edfac5c
Стефан <@-PC>
2017-03-07 11:52:21 +0200

    Add project files.

---
+++ StefanPeevBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StefanPeevBlog.Startup))]
+namespace StefanPeevBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
